65f99e5a87b54776c50f320917fda749a76ff06e
JinbeomKim <53505860+kjb512@users.noreply.github.com>
2022-05-21 22:15:11 +0900

    feat: S3업로드 구현

--- hackathon/src/main/java/cmc/hackathon/HackathonApplication.java
+++ hackathon/src/main/java/cmc/hackathon/HackathonApplication.java
@@ -2,14 +2,21 @@ package cmc.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class HackathonApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:aws.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(HackathonApplication.class, args);
+		new SpringApplicationBuilder(HackathonApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
--- hackathon/src/main/java/cmc/hackathon/config/auth/controller/OauthController.java
+++ hackathon/src/main/java/cmc/hackathon/config/auth/controller/OauthController.java
@@ -2,17 +2,32 @@ package cmc.hackathon.config.auth.controller;
 
 import cmc.hackathon.config.BaseResponse;
 import cmc.hackathon.config.auth.dto.OauthResponse;
+import cmc.hackathon.utils.S3Uploader;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+@RequiredArgsConstructor
 @RestController
 public class OauthController {
+
+    private final S3Uploader s3Uploader;
 
     @ApiOperation(value = "로그인 완료후 리다이렉트")
     @GetMapping("/auth")
     public BaseResponse<OauthResponse> jwtResponse(@RequestParam String jwt, @RequestParam Long id) {
         return new BaseResponse<>(new OauthResponse(id,jwt));
+    }
+
+    @PostMapping("/test")
+    public String test(@RequestParam("images") MultipartFile multipartFile) throws IOException {
+        s3Uploader.upload(multipartFile, "test");
+        return "";
     }
 }
